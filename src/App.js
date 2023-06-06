0cdc4a4c61c414bf8f3e0bca4ed530d9d7b78e99
ManishaLalage <manishalalage91@gmail.com>
2023-06-06 21:14:31 +0530

    Home Page completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+// import './App.css';
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import SocialLink from "./components/SocialLink";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Home/>
+     <SocialLink/>
+    </div>
+  );
+}
+
+export default App;
